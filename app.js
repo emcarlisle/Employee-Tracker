3dc279fbe08a11fc464c02894c7a70bd797cd9a7
Emily Carlisle <emilydeluna96@gmail.com>
2020-06-07 23:03:17 -0500

    adding inquirer prompt

--- app.js
+++ app.js
@@ -9,19 +9,70 @@ const connection = mysql.createConnection({
     database: "employeeinfo_db"
 });
 
-connection.connect(function (err) {
-    if (err) throw err;
-    //console.table(results);
-    afterConnection();
-    // connection.end();
-});
+//connection.connect(function (err) {
+//    if (err) throw err;
+//    //console.table(results);
+//    afterConnection();
+//    // connection.end();
+//});
+//
+//function afterConnection() {
+//    connection.query("SELECT * FROM products", function(err, res) {
+//        if (err) throw err;
+//        console.log(res);
+//        console.table(res);
+//        connection.end();
+//    });
+//}
 
-function afterConnection() {
-    connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-        console.log(res);
-        console.table(res);
-        connection.end();
-    });
+connection.connect();
+
+init();
+
+function init() {
+    const exit = false
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View All Departments",
+                "View All Roles",
+                "View All Employees",
+                "Add Department",
+                "Add Role",
+                "Add Employee",
+                "Update Employee Role",
+                "Exit"
+            ]
+
+        }).then((response) => {
+            switch (response.action) {
+                case "View All Departments":
+                    viewAllDepartments();
+                    break;
+                case "View All Roles":
+                    viewAllRoles();
+                    break;
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+                case "Add Department":
+                    addDepartment();
+                    break;
+                case "Add Role":
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+                case "Exit":
+                    exit = true;
+                    connection.end();
+                    break;
+            }
+        })
 }
-    
