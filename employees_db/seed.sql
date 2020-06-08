6cc9cdbc3fcc72dfe6f49afbecca75869b2dd0d2
Emily Carlisle <emilydeluna96@gmail.com>
2020-06-08 16:46:45 -0500

    condensing seed

--- employees_db/seed.sql
+++ employees_db/seed.sql
@@ -1,14 +1,17 @@
-INSERT INTO department (department_name) VALUES ("General Management");
-INSERT INTO department (department_name) VALUES ("Human Resources");
-INSERT INTO department (department_name) VALUES ("Finance");
-INSERT INTO department (department_name) VALUES ("Sales");
+INSERT INTO department (department_name)
+VALUES ("General Management"),("Human Resources"),("Finance"),("Sales");
+--INSERT INTO department (department_name) VALUES ;
+--INSERT INTO department (department_name) VALUES ;
+--INSERT INTO department (department_name) VALUES 
 
-INSERT INTO role (title, salary, department_id) VALUES ("Manager", 5000000, 1);
-INSERT INTO role (title, salary, department_id) VALUES ("Office Coordinator", 60000, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Accountant", 95000, 3);
-INSERT INTO role (title, salary, department_id) VALUES ("Sales Specialist", 100000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", 5000000, 1),("Office Coordinator", 60000, 2),("Accountant", 95000, 3),("Sales Specialist", 100000, 4);
+--INSERT INTO role (title, salary, department_id) VALUES ;
+--INSERT INTO role (title, salary, department_id) VALUES ;
+--INSERT INTO role (title, salary, department_id) VALUES ;
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Emily", "Carlisle", 1, NULL);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Ryan", "Reynolds", 2, NULL);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Jason", "Statham", 3, NULL);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Christian", "Bale", 4, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Emily", "Carlisle", 1, NULL),("Ryan", "Reynolds", 2, NULL),("Jason", "Statham", 3, NULL),("Christian", "Bale", 4, NULL);
+--INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ;
+--INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ;
+--INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ;
